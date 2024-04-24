8d8ae982acab8487c45d369fa7499e292933c96b
anthony.b.chung <anthony.b.chung@gmail.com>
2024-04-25 08:57:07 +1000

    nav_controller closeNav and openNav stimulus

--- app/javascript/controllers/nav_controller.js
+++ app/javascript/controllers/nav_controller.js
@@ -3,5 +3,34 @@ import { Controller } from "@hotwired/stimulus"
 // Connects to data-controller="nav"
 export default class extends Controller {
   connect() {
+    console.log("you are connected")
   }
+
+  static targets = ['closed']
+
+  initialize(){
+    this.closed = true
+  }
+
+  toggle(){
+    if(this.closed){
+      this.openNav()
+    } else {
+      this.closeNav()
+    }
+  }
+
+  openNav(){
+    this.closedTarget.classList.remove('h-0')
+    this.closedTarget.classList.add('h-screen')
+    this.closed=!this.closed
+  }
+
+  closeNav(){
+    this.closedTarget.classList.remove('h-screen')
+    this.closedTarget.classList.add('h-0')
+    this.closed=!this.closed
+  }
+
+  
 }
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
+
+  # Controller: StaticPages
   get 'contact', to: 'static_pages#contact', as: 'contact'
   get 'features', to: 'static_pages#features', as: 'features'
+
+  
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
