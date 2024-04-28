a3ef1d2eac9d63f50f48879e482cf940c402f6fe
anthony.b.chung <anthony.b.chung@gmail.com>
2024-04-28 22:03:57 +1000

    fixing new.html.erb

--- app/javascript/controllers/geolocation_controller.js
+++ app/javascript/controllers/geolocation_controller.js
@@ -19,12 +19,12 @@ export default class extends Controller {
   }
 
   success(position){
-      let longitude = position.coords.longitude
-      let latitude = position.coords.latitude
+      let longitude_pos = position.coords.longitude
+      let latitude_pos = position.coords.latitude
 
-      this.positionTarget.textContent = `long: ${longitude}, lat: ${latitude}`
-      this.longitudeTarget.value = longitude
-      this.latitudeTarget.value = latitude
+      this.positionTarget.textContent = `long: ${longitude_pos}, lat: ${latitude_pos}`
+      this.longitudeTarget.value = longitude_pos
+      this.latitudeTarget.value = latitude_pos
 
 
   }
