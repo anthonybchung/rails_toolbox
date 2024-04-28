ca14e22a33aec9b0139bc36009b0ea824e1b0024
anthony.b.chung <anthony.b.chung@gmail.com>
2024-04-28 21:57:48 +1000

    fixing new.html.erb

--- app/javascript/controllers/nav_controller.js
+++ app/javascript/controllers/nav_controller.js
@@ -3,7 +3,7 @@ import { Controller } from "@hotwired/stimulus"
 // Connects to data-controller="nav"
 export default class extends Controller {
   connect() {
-    console.log("you are connected")
+    console.log("nav")
   }
 
   static targets = ['closed']
